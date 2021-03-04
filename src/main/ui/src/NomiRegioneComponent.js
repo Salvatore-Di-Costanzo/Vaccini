f41637904d7fc226e3d38362cecb484a5155e5e4
Salvatore Di Costanzo <dicostanzosalvatore@yahoo.it>
2021-03-04 11:38:03 +0100

    Introduzione colori barre

--- src/main/ui/src/NomiRegioneComponent.js
+++ src/main/ui/src/NomiRegioneComponent.js
@@ -8,15 +8,56 @@ const CaricaDati = () => {
         const [chartData, setChartData] = useState([]);
         const [nomeRegioni, setNomiRegioni] = useState([]);
         const [contagi, setContagi] = useState([]);
+        var setColorPlugin = {
+
+            // We affect the `beforeUpdate` event
+            beforeUpdate: function(chart) {
+                var backgroundColor = [];
+                var borderColor = [];
+
+                // For every data we have ...
+                for (var i = 0; i < chart.config.data.datasets[0].data.length; i++) {
+
+                    // We generate a random color
+                    if(chart.config.data.datasets[0].get().data)
+                    var color = "rgba(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ",";
+
+                    // We push this new color to both background and border color arrays
+                    // .. a lighter color is used for the background
+                    backgroundColor.push(color + "0.2)");
+                    borderColor.push(color + "1)");
+                }
+
+                // We update the chart bars color properties
+                chart.config.data.datasets[0].backgroundColor = backgroundColor;
+                chart.config.data.datasets[0].borderColor = borderColor;
+            }
+        };
 
         const chart = () =>{
             let nomiRegioni = [];
             let contagiRegioni = [];
+            let backgroundColor = [];
+            let color;
             axios.get('http://localhost:8080/regioniContagi')
                 .then(res =>{
                     for(const dataObj of res.data){
                         nomiRegioni.push(dataObj.nomeRegione)
                         contagiRegioni.push(dataObj.numeroContagi)
+                    }
+                    for (var i = 0; i < contagiRegioni.length; i++) {
+
+                        // We generate a random color
+                        if(contagiRegioni[i] > 1500)
+                            color = 'rgb(255,8,8)';
+                        else if (contagiRegioni[i] >= 800)
+                            color = 'rgb(255,71,0)';
+                        else
+                            color = 'rgb(224,195,22)';
+
+                        // We push this new color to both background and border color arrays
+                        // .. a lighter color is used for the background
+                        backgroundColor.push(color);
                     }
                     setChartData({
                         labels: nomiRegioni,
@@ -24,9 +65,9 @@ const CaricaDati = () => {
                         datasets: [
                             {
                                 label: 'Numero Contagi',
-                                backgroundColor: 'rgb(255,219,8)', //Barre
-                                borderColor: 'rgb(46,88,46)', // Contorno Barre
-                                borderWidth: 1, // Spessore contorno
+                                backgroundColor: backgroundColor, //Barre
+                                //borderColor: 'rgb(46,88,46)', // Contorno Barre
+                                //borderWidth: 1, // Spessore contorno
                                 data: contagiRegioni
                             }
                         ]
