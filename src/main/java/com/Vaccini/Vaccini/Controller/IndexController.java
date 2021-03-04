a798317061253305453ed1e345f28eef7dd9ed75
Emanuele <iandoloemanuele7@gmail.com>
2021-03-04 17:34:35 +0100

    Refactor UI.

--- src/main/java/com/Vaccini/Vaccini/Controller/IndexController.java
+++ src/main/java/com/Vaccini/Vaccini/Controller/IndexController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class TestController {
+public class IndexController {
 
-    @GetMapping("/kitemm")
+    @GetMapping("/index")
         public String ciaone(){
-        return "kitemm";
+        return "graph";
     }
 }
