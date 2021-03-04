931a85465b965054c2d991949b4a362e3dcd07b4
Salvatore Di Costanzo <dicostanzosalvatore@yahoo.it>
2021-03-04 12:25:52 +0100

    Refactor UI

--- src/main/ui/src/NomiRegioneComponent.js
+++ src/main/ui/src/NomiRegioneComponent.js
@@ -1,38 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
-import {Bar, Line} from "react-chartjs-2";
+import {Bar} from "react-chartjs-2";
 
 
 
 const CaricaDati = () => {
         const [chartData, setChartData] = useState([]);
-        const [nomeRegioni, setNomiRegioni] = useState([]);
-        const [contagi, setContagi] = useState([]);
-        var setColorPlugin = {
-
-            // We affect the `beforeUpdate` event
-            beforeUpdate: function(chart) {
-                var backgroundColor = [];
-                var borderColor = [];
-
-                // For every data we have ...
-                for (var i = 0; i < chart.config.data.datasets[0].data.length; i++) {
-
-                    // We generate a random color
-                    if(chart.config.data.datasets[0].get().data)
-                    var color = "rgba(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ",";
-
-                    // We push this new color to both background and border color arrays
-                    // .. a lighter color is used for the background
-                    backgroundColor.push(color + "0.2)");
-                    borderColor.push(color + "1)");
-                }
-
-                // We update the chart bars color properties
-                chart.config.data.datasets[0].backgroundColor = backgroundColor;
-                chart.config.data.datasets[0].borderColor = borderColor;
-            }
-        };
 
         const chart = () =>{
             let nomiRegioni = [];
@@ -47,16 +20,12 @@ const CaricaDati = () => {
                     }
                     for (var i = 0; i < contagiRegioni.length; i++) {
 
-                        // We generate a random color
                         if(contagiRegioni[i] > 1500)
                             color = 'rgb(255,8,8)';
                         else if (contagiRegioni[i] >= 800)
                             color = 'rgb(255,71,0)';
                         else
                             color = 'rgb(224,195,22)';
-
-                        // We push this new color to both background and border color arrays
-                        // .. a lighter color is used for the background
                         backgroundColor.push(color);
                     }
                     setChartData({
