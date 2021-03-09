9793d09688296d47705c9f4d2d9ee603af8b4ee9
Salvatore Di Costanzo <dicostanzosalvatore@yahoo.it>
2021-03-09 16:55:05 +0100

    Aggiunto timer

---
+++ src/main/resources/static/js/timer.js
@@ -0,0 +1,24 @@
+let currentDate = new Date().toLocaleDateString();
+let countDownDate = new Date( currentDate +" 18:00:00").getTime();
+let distance;
+// Update the count down every 1 second
+let x = setInterval(function() {
+
+    // Get today's date and time
+    let now = new Date().getTime();
+    // Find the distance between now and the count down date
+    distance = countDownDate - now;
+    // Time calculations for days, hours, minutes and seconds
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("countDown").innerHTML = hours + "h "
+        + minutes + "m " + seconds + "s ";
+    // If the count down is over, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "Siamo quasi pronti! i dati si aggiorneranno a breve.";
+    }
+}, 1000);
