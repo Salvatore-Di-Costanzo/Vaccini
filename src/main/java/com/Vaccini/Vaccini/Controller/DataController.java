d8d687f0e8bd9abf9c54485464c293cd280492f4
Salvatore Di Costanzo <dicostanzosalvatore@yahoo.it>
2021-03-12 12:08:48 +0100

    Cache Dati

--- src/main/java/com/Vaccini/Vaccini/Controller/DataController.java
+++ src/main/java/com/Vaccini/Vaccini/Controller/DataController.java
@@ -9,13 +9,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.List;
+
+import java.util.HashMap;
 
 @Slf4j
 @Controller
 @CrossOrigin
 public class DataController {
+    private static HashMap<String, List<RegioneContagi>> cache = new HashMap<>();
 
     @Autowired
     Marshal marshal;
@@ -32,10 +35,12 @@ public class DataController {
 
     @GetMapping("/regioniContagi")
     @ResponseBody
-    public List<RegioneContagi> getDati(@RequestParam(value = "data", required = false, defaultValue = "") String data) throws IOException, SQLException {
-
-        return marshal.getDati(data);
-
+    public List<RegioneContagi> getDati(@RequestParam(value = "data", required = false, defaultValue = "") String data) throws IOException{
+        if (data.isEmpty())
+            data = LocalDate.now().toString();
+        if (cache.get(data) == null)
+            cache.put(data,marshal.getDati(data));
+        return cache.get(data);
     }
 
 }
