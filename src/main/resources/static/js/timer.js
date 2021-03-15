9c47a592197b93bafa16e4b5a17a948cf1d62f81
Emanuele <iandoloemanuele7@gmail.com>
2021-03-15 17:15:13 +0100

    FIX Timer

--- src/main/resources/static/js/timer.js
+++ src/main/resources/static/js/timer.js
@@ -1,24 +1,28 @@
-let currentDate = new Date().toLocaleDateString();
-let countDownDate = new Date( currentDate +" 18:00:00").getTime();
-let distance;
+// Set the date we're counting down to
+var today = new Date().toISOString().slice(0, 10)
+var countDownDate = new Date(today + " 18:00:00").getTime();
+
 // Update the count down every 1 second
-let x = setInterval(function() {
+var x = setInterval(function () {
 
     // Get today's date and time
-    let now = new Date().getTime();
-    // Find the distance between now and the count down date
-    distance = countDownDate - now;
-    // Time calculations for days, hours, minutes and seconds
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var now = new Date().getTime();
 
-    // Output the result in an element with id="demo"
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
     document.getElementById("countDown").innerHTML = hours + "h "
         + minutes + "m " + seconds + "s ";
-    // If the count down is over, write some text
-    if (hours === 23) {
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
         clearInterval(x);
-        document.getElementById("countDown").innerHTML = "<br>Siamo quasi pronti! <br> i dati si aggiorneranno a breve.";
+        document.getElementById("countDown").innerHTML = "I DATI SARANNO AGGIORNATI A BREVE";
     }
 }, 1000);
