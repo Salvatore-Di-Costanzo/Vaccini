7cb30d1a99c22a714edc9ed1481e9ba64fdbc8d5
Salvatore Di Costanzo <dicostanzosalvatore@yahoo.it>
2021-03-23 17:38:40 +0100

    Revert "Aggiunta WebFlux"
    This reverts commit 461f30e1

--- src/main/java/com/Vaccini/Vaccini/Controller/DataController.java
+++ src/main/java/com/Vaccini/Vaccini/Controller/DataController.java
@@ -39,7 +39,7 @@ public class DataController {
         return "waitingPage";
     }
 
-    @GetMapping("/regioniContagi")
+    @PostMapping("/regioniContagi")
     @ResponseBody
     public List<RegioneContagi> getDati(@RequestParam(value = "data", required = false) String data) throws IOException {
         if (data == null) {
@@ -52,12 +52,6 @@ public class DataController {
             cache.replace(data, marshal.getDati(data));
         }
         return cache.get(data);
-    }
-
-    @GetMapping("ciao")
-    @ResponseBody
-    public void ciaociao() throws IOException {
-        marshal.getDati("");
     }
 
 }
