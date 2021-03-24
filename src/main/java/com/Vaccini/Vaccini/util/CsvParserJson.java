9894227db22f1691f5824e6664e437005e60c68c
Salvatore Di Costanzo <dicostanzosalvatore@yahoo.it>
2021-03-24 10:15:42 +0100

    Refactor

--- src/main/java/com/Vaccini/Vaccini/util/CsvParserJson.java
+++ src/main/java/com/Vaccini/Vaccini/util/CsvParserJson.java
@@ -1,46 +1,34 @@
 package com.Vaccini.Vaccini.util;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Slf4j
 public class CsvParserJson {
 
 
     @PostMapping("/json")
-    public List<Map<?, ?>> jsonParser(@RequestParam(value = "url") String url) {
+    public List<Map<?, ?>> jsonParser(@RequestParam(value = "url") String url) throws IOException {
+        String CSV = IOUtils.toString(new URL(url).openStream(), StandardCharsets.UTF_8);
 
-        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("FILE_NAME")) {
-            byte dataBuffer[] = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                fileOutputStream.write(dataBuffer, 0, bytesRead);
-            }
-            File fileJson = new File("FILE_NAME");
-            return readObjectsFromCsv(fileJson);
-
-        } catch (IOException e) {
-            // handle exception
-        }
-
-        return null;
+        return readObjectsFromCsv(CSV);
     }
 
-    public static List<Map<?, ?>> readObjectsFromCsv(File file) throws IOException {
+    public static List<Map<?, ?>> readObjectsFromCsv(String CSV) throws IOException {
         CsvSchema bootstrap = CsvSchema.emptySchema().withHeader();
         CsvMapper csvMapper = new CsvMapper();
-        MappingIterator<Map<?, ?>> mappingIterator = csvMapper.reader(Map.class).with(bootstrap).readValues(file);
+        MappingIterator<Map<?, ?>> mappingIterator = csvMapper.reader(Map.class).with(bootstrap).readValues(CSV);
 
         return mappingIterator.readAll();
     }
