201007780c21b1864d794371ef43237a8239b9ae
Emanuele <iandoloemanuele7@gmail.com>
2021-03-24 09:18:23 +0100

    Pushettino

--- src/main/java/com/Vaccini/Vaccini/MarshalCSV/Marshal.java
+++ src/main/java/com/Vaccini/Vaccini/MarshalCSV/Marshal.java
@@ -2,44 +2,56 @@ package com.Vaccini.Vaccini.MarshalCSV;
 
 import com.Vaccini.Vaccini.DTO.RegioneContagi;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
 import java.io.*;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
 import java.util.List;
+
 
 @Slf4j
 @Component
 public class Marshal {
-
     public List<RegioneContagi> getDati(String data) throws IOException {
-        List<RegioneContagi> dati = new ArrayList<>();
-        boolean corretto = true;
         String dataMod = data.replace("-", "");
-        URL contagi = new URL("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-" + dataMod + ".csv");
-        URLConnection yc = contagi.openConnection();
-        BufferedReader in = null;
-        try {
-            in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
-        } catch (IOException e) {
-            corretto = false;
+        List<RegioneContagi> dati;
+        final String URL_LASTES = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-latest.csv";
+        String URL_DATA = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-" + dataMod + ".csv";
+        boolean corretto = true;
+
+        String URL = "http://localhost:8080/";
+
+        WebClient webClient = WebClient.builder()
+                .baseUrl(URL)
+                .build();
+
+        Flux<RegioneContagi> bean = webClient.post()
+                .uri(builder -> builder
+                        .path("json")
+                        .queryParam("url",URL_DATA)
+                        .build())
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToFlux(RegioneContagi.class);
+        dati = bean.collectList().block();
+
+                dati.forEach(System.out::println);
+
+
+        if (dati.isEmpty()){
+           bean = webClient.post()
+                    .uri(builder -> builder
+                            .path("json")
+                            .queryParam("url",URL_LASTES)
+                            .build())
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    .bodyToFlux(RegioneContagi.class);
+           dati = bean.collectList().block();
         }
-        if (!corretto) {
-            contagi = new URL("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-latest.csv");
-            yc = contagi.openConnection();
-            in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
-        }
-        String inputLine;
-        in.readLine();
-        while ((inputLine = in.readLine()) != null) {
-            String[] attributes = inputLine.split(",");
-            dati.add(new RegioneContagi(attributes[3], Integer.parseInt(attributes[12])));
-        }
-        if (!corretto) {
-            dati.add(new RegioneContagi("Error", 0));
-        }
+
         return dati;
     }
 }
